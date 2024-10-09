e6a10cec701bc96dd8236f009a26b7b873ab8286
peop1e12 <162907930+peop1e12@users.noreply.github.com>
2024-10-09 19:55:18 +0500

    загрузил скрипт Hello Word

---
+++ HeloWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Update");
+    }
+}
